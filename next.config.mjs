87d8f9657ed6d88eec043a5129df2ef9678e401c
Manon lopes <manon.lopes@railwai.com>
2026-01-17 14:18:03 +0100

    fix: Remove assetPrefix from next.config.mjs for export mode

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
   output: "export",
   typescript: {
     ignoreBuildErrors: true,
